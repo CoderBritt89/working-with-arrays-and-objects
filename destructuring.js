795d254010c6155b84471ae75ff96b50da64070e
CoderBritt89 <72882552+CoderBritt89@users.noreply.github.com>
2021-01-11 15:01:54 -0700

    update

--- destructuring.js
+++ destructuring.js
@@ -21,7 +21,10 @@ var carDetails = {
   Use object destructuring to save the property values from the object carDetails into new variables. 
 */
 
-//Code Here
+const {color} = carDetails;
+const {make} = carDetails;
+const {model} = carDetails;
+const {year} = carDetails;
 
 
 
